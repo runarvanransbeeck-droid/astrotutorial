b7fa3c966afe79ad00bd07bc04d0502a0f508e20
vanransbeeck <runar.van.ransbeeck@student.howest.be>
2025-10-15 18:00:07 +0200

    added randomized greeting and dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://runarvrastrotutorial.netlify.app/"
+  site: "https://runarvrastrotutorial.netlify.app/",
+  integrations: [preact()]
 });
